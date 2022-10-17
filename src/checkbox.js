4e6b47d282fe21cc129c2af0864af5c5cbd511c4
AVSCED <anantvikramshukla2022sep@cedcoss.com>
2022-10-17 15:42:30 +0530

    Updated

--- src/checkbox.js
+++ src/checkbox.js
@@ -1,5 +1,6 @@
 disp_arr = [];
 arrSplice = [];
+//To add product to cart
 function addProd() {
   var pcompany = document.getElementById("company").value;
 
@@ -18,16 +19,15 @@ function addProd() {
       product_price: pprice,
       flag: 0,
     };
-    console.log(cart);
     disp_arr.push(cart);
     dsiplay(disp_arr);
   }
 }
 let c;
 let s;
+// To display table
 function dsiplay(data) {
   c = 0;
-  console.log(data);
   s =
     " <tr><th>Company :</th> <th>Model :</th> <th>Memory GB:</th>  <th>Price :</th> <th>Action :</th></tr> ";
      if (data.length >= 1){
@@ -55,41 +55,16 @@ function dsiplay(data) {
   }
   document.getElementById("table1").innerHTML = s;
 }
-
+//To Create an array to store indexes to splice more than one element
 function selectedItems(g) {
   arrSplice.push(g);
 }
-
+//To delete the selected items
 function deleteProd() {
-  console.log(arrSplice);
   arrSplice.forEach(e => {
-    disp_arr.splice(e,1);
+
+    disp_arr.splice(disp_arr[e],1);
+
   });
   dsiplay(disp_arr);
 }
-// function dsiplay1() {
-//   s =
-//     " <tr><th>Company :</th> <th>Model :</th> <th>Memory GB:</th>  <th>Price :</th> <th>Action :</th></tr> ";
-
-//   s = disp_arr.forEach((value) => {
-//     s +=
-//       "<tr><td>" +
-//       value.product_id +
-//       "</td>" +
-//       "<td>" +
-//       value.product_name +
-//       "</td>" +
-//       "<td>" +
-//       value.product_memory +
-//       "</td>" +
-//       "<td>" +
-//       value.product_price +
-//       "</td>" +
-//       "<td> <input type='checkbox' unchecked id='" +
-//       c +
-//       "'onclick='selectedItems(this)'>" +
-//       "</td></tr>";
-//     c++;
-//     document.getElementById("table1").innerHTML = s;
-//   });
-// }
